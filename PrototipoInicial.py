25e239458d00e78cbf40bc836f067d81affa1948
Artur F <carpetblv@hotmail.com>
2025-08-12 11:31:54 -0300

    Atualização Prototipo

--- PrototipoInicial.py
+++ PrototipoInicial.py
@@ -1,1 +1,3 @@
 #Iniciando prototipo do codigo
+#cmd para entrar no chrome:
+#"C:\Program Files\Google\Chrome\Application\chrome.exe" --remote-debugging-port=9222 --user-data-dir="C:\Users\carpe\AppData\Local\Google\Chrome\User Data"
